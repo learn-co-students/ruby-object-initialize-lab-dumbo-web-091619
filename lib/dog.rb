7c3896d9e9535976041e592200c0d0475e155fcd
Gaida Escobar <gescobar129@gmail.com>
2019-09-18 22:10:30 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+require 'pry'
+
+class Dog
+    def initialize(name, breed="Mutt")
+        @name = name
+        @breed = breed
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+end
+
+
+
+#
